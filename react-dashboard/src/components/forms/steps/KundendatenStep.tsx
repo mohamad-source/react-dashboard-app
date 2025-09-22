6ae198ff7a3d09e198e17bc014b0befd07737cbb
mohamad-source <omeirat.mohamad@web.de>
2025-09-22 22:59:22 +0400

    Remove unused aktenApi import

--- react-dashboard/src/components/forms/steps/KundendatenStep.tsx
+++ react-dashboard/src/components/forms/steps/KundendatenStep.tsx
@@ -6,7 +6,7 @@ import { Label } from '@/components/ui/label'
 import { Alert, AlertDescription } from '@/components/ui/alert'
 import { Badge } from '@/components/ui/badge'
 import { supabase } from '../../../lib/supabase'
-import { aktenApi } from '../../../lib/aktenApi'
+//import { aktenApi } from '../../../lib/aktenApi'
 import {
   Upload,
   FileText,
